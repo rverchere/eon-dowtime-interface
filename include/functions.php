5964d1c8fba9dd8acfff4b4533eef72eec395b92
Rémi VERCHERE <remi.verchere@axians.com>
2018-11-07 17:41:49 +0100

    enc: add get/set downtime in php

--- include/functions.php
+++ include/functions.php
@@ -23,22 +23,72 @@ function getLabel($reference){
 }
 
 function createTableList($yamlConfPath) {
-        $confPath = scandir($yamlConfPath);
+    $confPath = scandir($yamlConfPath);
 
-        echo '<tr class=tr_head>'
-        echo '<td>'.getLabel(label.users_downtime.tablehead.app).'</td>'
-        echo '<td>'.getLabel(label.users_downtime.tablehead.desc).'</td>'
-        echo '<td>'.getLabel(label.users_downtime.tablehead.starttime).'</td>'
-        echo '<td>'.getLabel(label.users_downtime.tablehead.endtime).'</td>'
-        echo '<td></td>'
-        echo '</tr>'
+    echo '<tr class=tr_head>'
+    echo '<td>'.getLabel(label.users_downtime.tablehead.app).'</td>'
+    echo '<td>'.getLabel(label.users_downtime.tablehead.desc).'</td>'
+    echo '<td>'.getLabel(label.users_downtime.tablehead.starttime).'</td>'
+    echo '<td>'.getLabel(label.users_downtime.tablehead.endtime).'</td>'
+    echo '<td></td>'
+    echo '</tr>'
 }
 
-function add_dwtm() {
 
+function thrukGetDowntimes($server) {
+    $cookies = "user_name=admin; session_id=169014757; user_id=1; group_id=1; user_limitation=0";
+    $ch = curl_init('https://'.$server.'/thruk/r/downtimes');
+
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_COOKIE, $cookies);
+    $output = curl_exec($ch);
+    $rcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    curl_close($ch);
+
+    if($rcode == 200) {
+        return $output;
+    } else {
+        return"ERROR\n";
+    }
 }
 
-if ($_POST['d_add']) {
+function thrukSetDowntime($server, $hostname, $servicename, $details) {
+    /*
+    * Details are downtime values:
+    * - comment_data: downtime comment (required)
+    * - start_time (optionnal)
+    * - end_time (optionnal)
+    * - fixed (optionnal)
+    * - triggered_by (optionnal)
+    * - duration (optionnal)
+    * - comment_author (optionnal)
+    */
+    if($details =='') {
+        $fields = [
+            'comment_data' => 'PHP Test',
+            'comment_author' => 'Me'
+        ];
+    }
 
+    $cookies = "user_name=admin; session_id=169014757; user_id=1; group_id=1; user_limitation=0";
+    $url = 'https://'.$server.'/thruk/r/services/'.$hostname.'/'.$servicename.'/cmd/schedule_svc_downtime';
+    $ch = curl_init($url);
+
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_COOKIE, $cookies);
+
+    curl_setopt($ch, CURLOPT_POST, true );
+    curl_setopt($ch, CURLOPT_POSTFIELDS, $fields);
+
+    $output = curl_exec($ch);
+    $rcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+    curl_close($ch);
+
+    if($rcode == 200) {
+        return $output;
+    } else {
+        return"ERROR\n";
+    }
 }
+
 ?>
